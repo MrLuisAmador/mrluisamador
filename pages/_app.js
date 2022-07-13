29521381a3e99b7c345873f00055f5bedf84856b
Luis Amador <lamador@Luiss-MacBook-Pro.local>
2022-07-12 20:44:17 -0400

    adding google analytics to my portfolio project

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,27 @@ import Head from 'next/head'
 import '../styles/normalize.scss';
 import '../styles/variables.scss';
 import '../styles/global.scss';
+import Script from "next/script";
+import process from "eslint-config-next";
 
 function MyApp({ Component, pageProps }) {
   return (
   <Layout>
+    <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
+    <Script id="google-analytics" strategy="lazyOnload">
+      {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+    </Script>
+
     <Head>
       <link
           href="https://fonts.googleapis.com/css2?family=Alice&display=swap"
