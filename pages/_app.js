37e2e7e1375644b1c3bb3828925e8e39058e75de
Luis Amador <lamador@Luiss-MacBook-Pro.local>
2022-07-12 22:12:24 -0400

    adding the code snippet for everyone to see

--- pages/_app.js
+++ pages/_app.js
@@ -5,21 +5,20 @@ import '../styles/normalize.scss';
 import '../styles/variables.scss';
 import '../styles/global.scss';
 import Script from "next/script";
-import process from "eslint-config-next";
 
 function MyApp({ Component, pageProps }) {
   return (
   <Layout>
     <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=UA-70247510-1`}
     />
     <Script id="google-analytics" strategy="lazyOnload">
       {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            gtag('config', 'UA-70247510-1', {
               page_path: window.location.pathname,
             });
                 `}
