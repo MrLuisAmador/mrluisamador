001d13167c5625eabbbc0011fc346c09331448fc
Luis Amador <mrluisamador@gmail.com>
2023-10-02 01:53:59 -0400

    adding tag manager

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,8 @@
+import Script from 'next/script'
 import Nav from "../../components/nav/nav.js"
 import Footer from "../../components/footer/footer"
 import { Metadata } from 'next'
+const GTM_ID = process.env.GOOGLETAGKEY!
 
 import { Alice, Playfair_Display } from 'next/font/google';
 import '../../styles/global.scss';
@@ -30,6 +32,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${alice.variable} ${playfair_display.variable}`}>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body>
         <div className="w-[calc(100%-70px) mr-[70px] lg:w-[calc(100%-350px)] lg:mr-[350px]">
           <Nav />
@@ -40,6 +51,12 @@ export default function RootLayout({
               <Footer />
           </div>
         </div>
+
+        <noscript
+          dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }} 
+         />
       </body>
     </html>
   )
