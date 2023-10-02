a4042bfec28cabaee8a87dbdf76ccd6d5da171c5
Luis Amador <mrluisamador@gmail.com>
2023-10-02 02:00:46 -0400

    adding GA code

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -32,13 +32,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${alice.variable} ${playfair_display.variable}`}>
-      <Script id="google-tag-manager" strategy="afterInteractive">
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${GTM_ID}');
+        window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${GTM_ID});
         `}
       </Script>
       <body>
@@ -51,12 +55,6 @@ export default function RootLayout({
               <Footer />
           </div>
         </div>
-
-        <noscript
-          dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
-          }} 
-         />
       </body>
     </html>
   )
