d5bbc2b5181425842694c66a7efd218412141181
Luis Amador <mrluisamador@gmail.com>
2024-08-12 19:29:49 -0400

    adding underline to blog links

--- app/(home)/blogs/[slug]/page.tsx
+++ app/(home)/blogs/[slug]/page.tsx
@@ -1,31 +1,36 @@
-import { getBlog } from '@/sanity/lib/sanity-utils'
+import {getBlog} from '@/sanity/lib/sanity-utils'
 import {PortableText} from '@portabletext/react'
 import {RichTextsComponents} from '../../../../sanity/lib/RichTextsComponents'
-import Image from 'next/image';
-import urlFor from '@/sanity/lib/urlFor';
-import { Metadata } from 'next'
+import Image from 'next/image'
+import urlFor from '@/sanity/lib/urlFor'
+import {Metadata} from 'next'
 
 export const metadata: Metadata = {
   title: 'Blog | Luis Amador Portfolio',
-  description: 'Portfolio Website And Blog'
+  description: 'Portfolio Website And Blog',
 }
 
 type Props = {
-  params: { slug: string }
+  params: {slug: string}
 }
 
 const Blog = async ({params: {slug}}: Props) => {
-  
-  const blog =  await getBlog(slug)
-    return (
-      <article className="py-16">
-        <h1 className="text-center text-4xl mb-12">{blog.title}</h1>
-        <div className="px-5 py-16 mx-auto max-w-4xl xl:shadow xl:shadow-black xl:rounded bg-white">
-          <Image className="pb-16" alt={blog.title} src={urlFor(blog.mainImage).url()} width={896} height={800} />
-          <PortableText value={blog.body} components={RichTextsComponents} />
-        </div>
-      </article>
-    )
+  const blog = await getBlog(slug)
+  return (
+    <article className="py-16">
+      <h1 className="text-center text-4xl mb-12">{blog.title}</h1>
+      <div className="px-5 py-16 mx-auto max-w-4xl xl:shadow xl:shadow-black xl:rounded bg-white">
+        <Image
+          className="pb-16 underline"
+          alt={blog.title}
+          src={urlFor(blog.mainImage).url()}
+          width={896}
+          height={800}
+        />
+        <PortableText value={blog.body} components={RichTextsComponents} />
+      </div>
+    </article>
+  )
 }
 
 export default Blog
