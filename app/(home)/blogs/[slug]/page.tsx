264e563858fcf01196f5eb8cd8f0c9cad04a76ef
Luis Amador <mrluisamador@gmail.com>
2024-09-05 13:33:37 -0400

    custom adsense code snippet

--- app/(home)/blogs/[slug]/page.tsx
+++ app/(home)/blogs/[slug]/page.tsx
@@ -56,6 +56,21 @@ const Blog = async ({params: {slug}}: Props) => {
         data-full-width-responsive="true"
       ></ins>
       <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      {/* <!-- In feed ads --> */}
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2988961562271197"
+        crossOrigin="anonymous"
+      ></script>
+      <ins
+        className="adsbygoogle"
+        style={{display: 'block'}}
+        data-ad-format="fluid"
+        data-ad-layout-key="+3d+ql+42-68+4l"
+        data-ad-client="ca-pub-2988961562271197"
+        data-ad-slot="5724302338"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
       <Link className="mx-2 mb-4 xl:mx-6 xl:mb-6 inline-block" href="/blogs">
         Back to Blogs
       </Link>
