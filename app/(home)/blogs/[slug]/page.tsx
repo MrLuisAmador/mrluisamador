b276bd1eb2b567b6e1627ec9bc710e96e053efb0
Luis Amador <mrluisamador@gmail.com>
2024-09-12 01:01:55 -0400

    removing adsense code

--- app/(home)/blogs/[slug]/page.tsx
+++ app/(home)/blogs/[slug]/page.tsx
@@ -41,51 +41,7 @@ const Blog = async ({params: {slug}}: Props) => {
         type="application/ld+json"
         dangerouslySetInnerHTML={{__html: JSON.stringify(jsonLd)}}
       />
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2988961562271197"
-        crossOrigin="anonymous"
-      ></script>
-      {/* <!-- In feed ads --> */}
-      <ins
-        className="adsbygoogle"
-        style={{display: 'block'}}
-        data-ad-client="ca-pub-2988961562271197"
-        data-ad-slot="6682160780"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-      {/* <!-- In feed ads --> */}
-      {/* <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2988961562271197"
-        crossOrigin="anonymous"
-      ></script>
-      <ins
-        className="adsbygoogle"
-        style={{display: 'block'}}
-        data-ad-format="fluid"
-        data-ad-layout-key="+3d+ql+42-68+4l"
-        data-ad-client="ca-pub-2988961562271197"
-        data-ad-slot="5724302338"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script> */}
-      {/* <!-- In feed ads --> */}
-      {/* <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2988961562271197"
-        crossOrigin="anonymous"
-      ></script>
-      <ins
-        className="adsbygoogle"
-        style={{display: 'block', textAlign: 'center'}}
-        data-ad-layout="in-article"
-        data-ad-format="fluid"
-        data-ad-client="ca-pub-2988961562271197"
-        data-ad-slot="7549014731"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script> */}
+
       <Link className="mx-2 mb-4 xl:mx-6 xl:mb-6 inline-block" href="/blogs">
         Back to Blogs
       </Link>
--- sanity/lib/RichTextsComponents.tsx
+++ sanity/lib/RichTextsComponents.tsx
@@ -1,67 +1,60 @@
-import Image from  'next/image'
+import Image from 'next/image'
 import urlFor from './urlFor'
 
 export const RichTextsComponents = {
-    types: {
-        image: ({value}: any ) => {
-            return (
-                <div className="text-center pb-10">
-                    <Image className="inline" src={urlFor(value).url()} alt="Blog Image" width={650} height={700} />
-                </div>
-            )
-        }
+  types: {
+    image: ({value}: any) => {
+      return (
+        <div className="text-center pb-10">
+          <Image
+            className="inline"
+            src={urlFor(value).url()}
+            alt="Blog Image"
+            width={650}
+            height={700}
+          />
+        </div>
+      )
     },
-    block: {
-        h2: ({children}: any) => {
-            return (
-                <h1 className="mb-[2px] text-2xl">{children}</h1>
-            )
-        },
-        blockquote: ({children}: any) => {
-            return (
-                <blockquote className="">{children}</blockquote>
-            )
-        },
-        normal: ({children}: any) => {
-            return (
-                <p className="mb-5 text-lg">{children}</p>
-            )
-        }
+  },
+  block: {
+    h2: ({children}: any) => {
+      return <h1 className="mb-[2px] text-2xl">{children}</h1>
     },
-    list: {
-        number: ({children}: any) => {
-            return (
-                <ol className="mb-5 list-decimal pl-12">{children}</ol>
-            )
-        },
-        bullet: ({children}: any) => {
-            return (
-                <ul className="mb-5 list-disc pl-12">{children}</ul>
-            )
-        }
-    },  
-    listItem: {
-        number: ({children}: any) => {
-            return (
-                <li className="pb-4">{children}</li>
-            )
-        },
-        bullet: ({children}: any) => {
-            return (
-                <li className="pb-4">{children}</li>
-            )
-        }
+    blockquote: ({children}: any) => {
+      return <blockquote className="">{children}</blockquote>
     },
-    marks: {
-        link: ({value, children}: any) => {
-            // const target = (value?.href || '').startsWith('http') ? '_blank' : undefined
-            return (
-            //   <a className="underline decoration-solid" href={value?.href} target={target} rel={target === '_blank' && 'noindex nofollow'}>
-            //     {children}
-            //   </a>
-               <a className="underline decoration-solid" href={value?.href} target="_blank">{children}</a>
-            )
-          },
-    }         
+    normal: ({children}: any) => {
+      return <p className="mb-5 text-lg">{children}</p>
+    },
+  },
+  list: {
+    number: ({children}: any) => {
+      return <ol className="mb-5 list-decimal pl-12">{children}</ol>
+    },
+    bullet: ({children}: any) => {
+      return <ul className="mb-5 list-disc pl-12">{children}</ul>
+    },
+  },
+  listItem: {
+    number: ({children}: any) => {
+      return <li className="pb-4">{children}</li>
+    },
+    bullet: ({children}: any) => {
+      return <li className="pb-4">{children}</li>
+    },
+  },
+  marks: {
+    link: ({value, children}: any) => {
+      // const target = (value?.href || '').startsWith('http') ? '_blank' : undefined
+      return (
+        //   <a className="underline decoration-solid" href={value?.href} target={target} rel={target === '_blank' && 'noindex nofollow'}>
+        //     {children}
+        //   </a>
+        <a className="underline decoration-solid" href={value?.href} target="_blank">
+          {children}
+        </a>
+      )
+    },
+  },
 }
-  
