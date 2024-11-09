829b9604d5f70e83ef63af95f0dcaad81845e23c
Luis Amador <luisamador@Luiss-MacBook-Pro.local>
2024-11-09 00:32:08 -0500

    return of Sanity

--- app/(home)/blogs/[slug]/page.tsx
+++ app/(home)/blogs/[slug]/page.tsx
@@ -1,48 +1,24 @@
-import Link from 'next/link'
+import {getBlog} from '@/sanity/lib/sanity-utils'
+import {PortableText} from '@portabletext/react'
+import {RichTextsComponents} from '../../../../sanity/lib/RichTextsComponents'
 import Image from 'next/image'
-import {getWixClient} from '@/wix/useWixClientServer'
-import RichContentViewer from '@/wix/RichContentViewer'
-import {toDraft} from 'ricos-content/libs/toDraft'
-import {media} from '@wix/sdk'
+import urlFor from '@/sanity/lib/urlFor'
 
 type Props = {
-  params: Promise<{slug: string}>
+  params: {slug: string}
 }
 
-export async function generateMetadata(props: Props) {
-  const params = await props.params;
+export async function generateMetadata({params: {slug}}: Props) {
+  const post = await getBlog(slug)
 
-  const {
-    slug
-  } = params;
-
-  const queryWixBlogs: any = await getWixClient()
-
-  const {items: blog} = await queryWixBlogs.items
-    .queryDataItems({
-      dataCollectionId: 'blogPost',
-      referencedItemOptions: [
-        {
-          fieldName: 'refAuthors',
-        },
-        {
-          fieldName: 'refCategories',
-        },
-      ],
-    })
-    .eq('slug', slug)
-    .find()
-
-  const post = blog![0]
-
-  const metaURL = media.getImageUrl(post.data.image).url
-  const metaAuthor = post.data.refAuthors.title
-  const metaDate = post.data._updatedDate.$date
-  const metaTile = post.data.title
+  const metaURL = urlFor(post.mainImage).url()
+  const metaAuthor = post.author.name
+  const metaDate = post._updatedAt
+  const metaTile = post.title
 
   return {
     title: metaTile,
-    description: post.data.description,
+    description: post.description,
     openGraph: {
       images: [
         {
@@ -70,62 +46,20 @@ export async function generateMetadata(props: Props) {
   }
 }
 
-const Blog = async (props: Props) => {
-  const params = await props.params;
-
-  const {
-    slug
-  } = params;
-
-  const queryWixBlogs: any = await getWixClient()
-
-  const {items: blog} = await queryWixBlogs.items
-    .queryDataItems({
-      dataCollectionId: 'blogPost',
-    })
-    .eq('slug', slug)
-    .find()
-
-  const post = blog![0]
-
-  const jsonLd = {
-    '@context': 'https://schema.org',
-    '@type': 'Article',
-    headline: post.data.title,
-    datePublished: post.publishedAt,
-    description: post.data.description,
-    author: [
-      {
-        '@type': 'Person',
-        name: 'Luis Amador',
-        url: `https://www.mrluisamador.com/blogs/${blog.slug}`,
-        image: `metaURL`,
-      },
-    ],
-  }
-
+const Blog = async ({params: {slug}}: Props) => {
+  const blog = await getBlog(slug)
   return (
     <article className="py-16">
-      {/* Add JSON-LD to your page */}
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{__html: JSON.stringify(jsonLd)}}
-      />
-
-      <Link className="mx-2 mb-4 xl:mx-6 xl:mb-6 inline-block" href="/blogs">
-        Back to Blogs
-      </Link>
-      <h1 className="text-center text-4xl xl:mb-12">{post.data.title}</h1>
-
-      <div className="px-5 pt-14 xl:py-16 mx-auto max-w-4xl xl:shadow xl:shadow-black xl:rounded bg-white">
+      <h1 className="text-center text-4xl mb-12">{blog.title}</h1>
+      <div className="px-5 py-16 mx-auto max-w-4xl xl:shadow xl:shadow-black xl:rounded bg-white">
         <Image
-          src={media.getImageUrl(post.data.image).url}
-          width="896"
-          height="800"
-          alt={`media.getImageUrl(post.data.image).altText`}
-          className="pb-16"
+          className="pb-16 underline"
+          alt={blog.title}
+          src={urlFor(blog.mainImage).url()}
+          width={896}
+          height={800}
         />
-        <RichContentViewer content={toDraft(post.data!.content) as {blocks: any; entityMap: any}} />
+        <PortableText value={blog.body} components={RichTextsComponents} />
       </div>
     </article>
   )
--- app/(home)/blogs/page.tsx
+++ app/(home)/blogs/page.tsx
@@ -1,61 +1,42 @@
 import Link from 'next/link'
-import {Metadata} from 'next'
-import {getWixClient} from '@/wix/useWixClientServer'
 import Image from 'next/image'
-import {media} from '@wix/sdk'
+import {Metadata} from 'next'
+import {getBlogs} from '@/sanity/lib/sanity-utils'
+import urlFor from '@/sanity/lib/urlFor'
 
 export const metadata: Metadata = {
-  title: 'Blogs',
+  title: 'Blog',
   description: 'Portfolio Website And Blog',
 }
 
 export default async function Blogs() {
-  const myWixBlogs: any = await getWixClient()
-
-  const {items: blogs} = await myWixBlogs.items
-    .queryDataItems({
-      dataCollectionId: 'blogPost',
-      referencedItemOptions: [
-        {
-          fieldName: 'refAuthors',
-        },
-        {
-          fieldName: 'refCategories',
-        },
-      ],
-    })
-    .descending('order')
-    .find()
+  const blogs = await getBlogs()
 
   return (
     <section className="h-full py-20 px-5">
       <h1 className="text-5xl text-center mb-20">Blogs</h1>
 
       <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {blogs.map((blog: any) => (
-          <li key={blog.data._id} className="shadow shadow-black rounded bg-white">
-            {/* <p>Name {blog.data.refAuthors.title}</p> */}
-            {/* <p>Name {blog.data.refCategories.title}</p> */}
-            {/* <p className="">{formatDate(new Date(blog.data!._createdDate?.$date))}</p> */}
-            {/* <p className="">{formatDate(new Date(blog.data!._updatedDate?.$date))}</p> */}
+        {blogs.map((blog) => (
+          <li key={blog._id} className="shadow shadow-black rounded bg-white">
             <article className="py-5 px-4">
               <div className="">
                 <Image
-                  src={media.getImageUrl(blog.data!.image).url}
-                  width="896"
-                  height="800"
-                  alt={`media.getImageUrl(blog.data.image).altText`}
                   className="pb-4"
+                  alt={blog.title}
+                  src={urlFor(blog.mainImage).url()}
+                  width={600}
+                  height={400}
                 />
               </div>
               <h2 className="text-center pb-4 font-bold text-xl">
-                <Link href={`/blogs/${blog.data.slug}`}>{blog.data.title}</Link>
+                <Link href={`/blog/${blog.slug}`}>{blog.title}</Link>
               </h2>
-              <p className="pb-4">{blog.data.description}</p>
+              <p className="pb-4">{blog.description}</p>
               <div className="text-center">
                 <Link
                   className="border border-solid border-black text-black py-2.5 px-4 inline-block rounded text-xl hover:bg-black/[.15] transition-colors"
-                  href={`/blogs/${blog.data.slug}`}
+                  href={`/blogs/${blog.slug}`}
                 >
                   Read More...
                 </Link>
