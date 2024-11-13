8877e2b4348957f1361a8ce3871b229ff7746887
Luis Amador <mrluisamador@gmail.com>
2024-11-13 01:46:44 -0500

    fixing the eslint errors

--- app/(home)/thankyou/gohome.tsx
+++ app/(home)/thankyou/gohome.tsx
@@ -7,10 +7,13 @@ const GoHome = () => {
   const router = useRouter()
 
   useEffect(() => {
-    setTimeout(() => {
+    const timeout = setTimeout(() => {
       router.push('/')
     }, 10000)
-  }, [])
+
+    // Cleanup timeout on unmount
+    return () => clearTimeout(timeout)
+  }, [router]) // Add router to dependencies
 
   return (
     <section
@@ -20,7 +23,6 @@ const GoHome = () => {
       <div className="md:w-4/5">
         <div className="text-center">
           <h1 className="text-4xl mb-5">Thank You for reaching out!</h1>
-
           <p className="text-lg mb-12">I will be in contact with you shortly.</p>
         </div>
       </div>
