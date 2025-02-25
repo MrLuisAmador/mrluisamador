794366f2da93ad2a47a8dc12f69a33fb60abbbbf
Luis Amador <mrluisamador@gmail.com>
2025-02-24 21:58:01 -0500

    fixing comma issue

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h2 className="text-4xl font-bold">Not Found</h2>
-      <p className="text-gray-500 mt-2">Sorry, we couldn't find that page.</p>
+      <p className="text-gray-500 mt-2">Sorry, we could not find that page.</p>
       <Link href="/">Return Home</Link>
     </div>
   )
