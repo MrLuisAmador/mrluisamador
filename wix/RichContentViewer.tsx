aa572370a7f5eb939fe60832e04d9dc30209f444
Luis Amador <mrluisamador@gmail.com>
2025-04-23 20:07:06 -0400

    adding more CMS Wix plugins

--- wix/RichContentViewer.tsx
+++ wix/RichContentViewer.tsx
@@ -1,9 +1,24 @@
 'use client'
 
-import {pluginGallery, pluginImage, quickStartViewerPlugins, RicosViewer} from '@wix/ricos'
+import {
+  pluginGallery,
+  pluginImage,
+  quickStartViewerPlugins,
+  RicosViewer,
+  pluginDivider,
+  pluginCodeBlock,
+  pluginTable,
+} from '@wix/ricos'
 
 const RichContentViewer = ({content}: {content: any}) => {
-  const plugins = [...quickStartViewerPlugins(), pluginGallery(), pluginImage()]
+  const plugins = [
+    ...quickStartViewerPlugins(),
+    pluginGallery(),
+    pluginImage(),
+    pluginDivider(),
+    pluginCodeBlock(),
+    pluginTable(),
+  ]
 
   return <RicosViewer content={content} plugins={plugins} />
 }
